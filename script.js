bc4df3d917cc777d709ea8db7f35f99d38697af1
zzzael <81818542+zzzael@users.noreply.github.com>
2024-07-12 14:17:43 +0100

    Added backend

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const hashKeyInput = document.getElementById('hash-key').value;
+    const errorMessage = document.getElementById('error-message');
+
+    fetch('./datakeys.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const validKeys = Object.values(data);
+            if (validKeys.includes(hashKeyInput)) {
+                window.location.href = 'sound.html';
+            } else {
+                errorMessage.textContent = 'Incorrect HASH-KEY';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching or parsing JSON:', error);
+            errorMessage.textContent = 'Error validating HASH-KEY';
+        });
+});
