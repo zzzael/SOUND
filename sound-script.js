778e740adde999d9a3e06d63d4b98ab0d862b753
zzzael <81818542+zzzael@users.noreply.github.com>
2024-07-12 16:43:15 +0100

    Update sound-script.js

--- sound-script.js
+++ sound-script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const audio = new Audio('puresouls.mp3');
-    audio.volume = 0.5; // Set initial volume to 60%
-
+    audio.volume = 0.4; // Set the volume to 60%
+    
     // Function to fade in the audio
     const fadeInAudio = () => {
         let volume = 0;
@@ -15,25 +15,12 @@ document.addEventListener('DOMContentLoaded', function() {
             audio.volume = volume;
         }, 120); // 20ms interval for smooth fade-in over 2 seconds
     };
-
-    // Function to fade out the audio
-    const fadeOutAudio = () => {
-        let volume = 0.6;
-        const fadeOutInterval = setInterval(() => {
-            volume -= 0.01;
-            if (volume <= 0) {
-                volume = 0;
-                clearInterval(fadeOutInterval);
-                audio.pause();
-            }
-            audio.volume = volume;
-        }, 120); // 100ms interval for smooth fade-out over 10 seconds
-    };
-
+    
     audio.addEventListener('ended', function() {
         setTimeout(() => {
-            fadeOutAudio();
-        }, 3000); // 3 seconds pause before fade-out
+            fadeInAudio();
+            audio.play();
+        }, 3000); // 3 seconds pause
     });
 
     fadeInAudio(); // Start fade in when the page loads
