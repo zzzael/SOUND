02e77ee1d0d2833cfc54fff0b0c284c49992b4e3
zzzael <81818542+zzzael@users.noreply.github.com>
2024-07-13 18:02:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const hashKeyInput = document.getElementById('hash-key').value;
+    const hashKeyInput = document.getElementById('hash-key').value.trim();
     const errorMessage = document.getElementById('error-message');
 
-    fetch('./datakeys.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const validKeys = Object.values(data);
-            if (validKeys.includes(hashKeyInput)) {
-                window.location.href = 'sound.html';
-            } else {
-                errorMessage.textContent = 'Incorrect HASH-KEY';
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching or parsing JSON:', error);
-            errorMessage.textContent = 'Error validating HASH-KEY';
-        });
+    const fixedKey = 'SOUND';
+
+    if (hashKeyInput === fixedKey) {
+        window.location.href = 'loading.html';
+    } else {
+        fetch('./datakeys.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const validKeys = Object.values(data);
+                if (validKeys.includes(hashKeyInput)) {
+                    window.location.href = 'sound.html';
+                } else {
+                    errorMessage.textContent = '???';
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching or parsing JSON:', error);
+                errorMessage.textContent = 'Error validating HASH-KEY';
+            });
+    }
 });
