06c120e0e45be06b5b5fc2d3d7f2db00e1c2d083
zzzael <81818542+zzzael@users.noreply.github.com>
2024-07-14 20:27:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const hashKeyInput = document.getElementById('hash-key').value.trim();
+    const hashKeyInput = document.getElementById('hash-key').value;
     const errorMessage = document.getElementById('error-message');
 
-    const fixedKey = 'SOUND';
-
-    if (hashKeyInput === fixedKey) {
-        window.location.href = 'loading.html';
-    } else {
-        fetch('./datakeys.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const validKeys = Object.values(data);
-                if (validKeys.includes(hashKeyInput)) {
-                    window.location.href = 'sound.html';
-                } else {
-                    errorMessage.textContent = '???';
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching or parsing JSON:', error);
-                errorMessage.textContent = 'Error validating HASH-KEY';
-            });
+    if (hashKeyInput === 'kbghu4ug3tr3ui42424b2') {
+        window.location.href = 'beta.html';
+        return;
     }
+    if (hashKeyInput === 'Yandhi') {
+        window.location.href = 'catalogue.html';
+        return;
+    }
+
+    fetch('./datakeys.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const validKeys = Object.values(data);
+            if (validKeys.includes(hashKeyInput)) {
+                window.location.href = 'loading.html';
+            } else {
+                errorMessage.textContent = 'Incorrect HASH-KEY';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching or parsing JSON:', error);
+            errorMessage.textContent = 'Error validating HASH-KEY';
+        });
 });
